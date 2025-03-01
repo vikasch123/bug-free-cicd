8a7ab65ce543d37f586686e8b29e950a76cc19f8
Vikas kumar <120211610+vikasch123@users.noreply.github.com>
2025-03-01 19:30:50 +0000

    updated .yml

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,11 +1,18 @@
 import pytest
-from app import app
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app  # Now it should correctly find app.py
 
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
     with app.test_client() as client:
-        yield client
+        with app.app_context():
+            yield client
 
 def test_home_page(client):
     response = client.get("/")
